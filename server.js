44b9911ca205e01062fb65dd38eed9e530758e19
Binh Tran <tranlenhatbinh191988@gmail.com>
2020-04-22 09:41:32 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const connectDB = require('./config/db')
+const cors = require('cors') // Fix Access-Control-Allow-Origin error
+app.use(cors())
+app.get('/', (req, res) =>  res.send('API running'))
+
+// Connect Database
+connectDB()
+
+// Init middleware
+app.use(express.json()) // read req.body
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
