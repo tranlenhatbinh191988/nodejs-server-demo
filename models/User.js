1f5cc11ceb61e9796569c535f5ff16a05023fe70
Van Rin <rin.to@enjoyworks.co.kr>
2020-04-22 14:01:55 +0700

    Implement api for login account

--- models/User.js
+++ models/User.js
@@ -1,26 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   avatar: {
-    type: String
+    type: String,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+});
 
-module.exports = User = mongoose.model('user', UserSchema)
+module.exports = User = mongoose.model("user", UserSchema);
