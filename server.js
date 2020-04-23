2eec6804e1b4c037e6f4512a62b668cd1f254187
tranlenhatbinh191988 <tranlenhatbinh191988@gmail.com>
2020-04-23 13:03:08 +0700

    Implement creating post

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ connectDB();
 app.use(express.json()); // read req.body
 
 // Define Routes
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/profile", require("./routes/api/profile"))
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
