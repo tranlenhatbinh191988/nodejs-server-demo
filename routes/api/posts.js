e2a30561e3498c85ad1b24c30152412aef65f0a4
Van Rin <rin.to@enjoyworks.co.kr>
2020-04-24 10:08:28 +0700

    Api for get list post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,42 +1,51 @@
-const express = require('express')
-const router = express.Router()
-const { check, validationResult } = require('express-validator')
-const auth = require('../../middleware/auth')
-const Post = require('../../models/Post')
-const User = require('../../models/User')
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const Post = require("../../models/Post");
+const User = require("../../models/User");
 
 // @route POST api/posts
 // @desc Create a post
 // @access Private
 
 router.post(
-  '/create',
-  [
-    auth,
-    [
-      check('text', 'Text is required').not().isEmpty()
-    ]
-  ],
-  async (req,res) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-      return res.status(400).json({ errors: errors.array()})
+  "/create",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
     try {
-      const user = await User.findById(req.user.id).select('-password')
+      const user = await User.findById(req.user.id).select("-password");
       const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
-        user: req.user.id
-      })
-      const post = await newPost.save()
-      res.json(post)
-    } catch(err){
-      console.log(err.message)
-      res.status(500).send('Server error')
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
     }
-  })
+  }
+);
 
-module.exports = router
+// @route GET api/posts
+// @desc get list post
+// @access Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    res.status(500).send(`Server Error: ${error}`);
+  }
+});
+
+module.exports = router;
